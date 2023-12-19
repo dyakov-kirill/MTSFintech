29dfd868cdb6259e543cfe8b53f9509ffbfef0ed
Kirill Dyakov <kira-d03@mail.ru>
2023-12-19 10:49:59 +0300

    added random animals

--- src/services/CreateAnimalService.java
+++ src/services/CreateAnimalService.java
@@ -1,15 +1,34 @@
 package services;
 
-import animals.AbstractAnimal;
-import animals.Cat;
+import animals.*;
+
+import java.util.Random;
 
 public interface CreateAnimalService {
     default void createAnimals() {
         int i = 0;
+        Random rand = new Random();
         System.out.print("Default method\n");
         while (i++ < 10) {
-            AbstractAnimal animal = new Cat();
-            System.out.printf("%d, New unique animal created! Hash: %d\n", i, animal.hashCode());
+            AbstractAnimal animal;
+            switch (rand.nextInt(0, 4)) {
+                case 0 -> {
+                    animal = new Dog();
+                    System.out.printf("%d, New dog created! Hash: %d\n", i, animal.hashCode());
+                }
+                case 1 -> {
+                    animal = new Cat();
+                    System.out.printf("%d, New cat created! Hash: %d\n", i, animal.hashCode());
+                }
+                case 2 -> {
+                    animal = new Wolf();
+                    System.out.printf("%d, New wolf created! Hash: %d\n", i, animal.hashCode());
+                }
+                case 3 -> {
+                    animal = new Shark();
+                    System.out.printf("%d, New shark created! Hash: %d\n", i, animal.hashCode());
+                }
+            }
         }
     }
 }
--- src/services/CreateAnimalServiceImpl.java
+++ src/services/CreateAnimalServiceImpl.java
@@ -1,26 +1,61 @@
 package services;
 
-import animals.AbstractAnimal;
-import animals.Cat;
-import animals.Dog;
-import animals.Wolf;
+import animals.*;
 
-public class CreateAnimalServiceImpl implements CreateAnimalService{
+import java.util.Random;
+
+public class CreateAnimalServiceImpl implements CreateAnimalService {
     @Override
     public void createAnimals() {
+        Random rand = new Random();
         System.out.print("Overrided method\n");
         int i = 0;
         do {
-            AbstractAnimal animal = new Dog();
-            System.out.printf("%d, New unique animal created! Hash: %d\n", i, animal.hashCode());
-        } while(i++ < 9);
+            AbstractAnimal animal;
+            switch (rand.nextInt(0, 4)) {
+                case 0 -> {
+                    animal = new Dog();
+                    System.out.printf("%d, New dog created! Hash: %d\n", i, animal.hashCode());
+                }
+                case 1 -> {
+                    animal = new Cat();
+                    System.out.printf("%d, New cat created! Hash: %d\n", i, animal.hashCode());
+                }
+                case 2 -> {
+                    animal = new Wolf();
+                    System.out.printf("%d, New wolf created! Hash: %d\n", i, animal.hashCode());
+                }
+                case 3 -> {
+                    animal = new Shark();
+                    System.out.printf("%d, New shark created! Hash: %d\n", i, animal.hashCode());
+                }
+            }
+        } while (i++ < 9);
     }
 
     public void createAnimals(int N) {
+        Random rand = new Random();
         System.out.print("Overloaded method\n");
         for (int i = 0; i < N; i++) {
-            AbstractAnimal animal = new Wolf();
-            System.out.printf("%d, New unique animal created! Hash: %d\n", i, animal.hashCode());
+            AbstractAnimal animal;
+            switch (rand.nextInt(0, 4)) {
+                case 0 -> {
+                    animal = new Dog();
+                    System.out.printf("%d, New dog created! Hash: %d\n", i, animal.hashCode());
+                }
+                case 1 -> {
+                    animal = new Cat();
+                    System.out.printf("%d, New cat created! Hash: %d\n", i, animal.hashCode());
+                }
+                case 2 -> {
+                    animal = new Wolf();
+                    System.out.printf("%d, New wolf created! Hash: %d\n", i, animal.hashCode());
+                }
+                case 3 -> {
+                    animal = new Shark();
+                    System.out.printf("%d, New shark created! Hash: %d\n", i, animal.hashCode());
+                }
+            }
         }
     }
 
